0c823b9a0393438ef244b21e0ebf1c58eefdeece
David <bolotaev-david@mail.ru>
2019-04-14 21:31:13 +0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Давид on 14/04/2019.
+//  Copyright © 2019 Давид. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
